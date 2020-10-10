22006c987ec71b7e492627a39497363d9bc1a613
Daniel <mineminecraftpe687@gmail.com>
2020-10-10 16:59:43 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.body.innerHTML += '<p style="position:fixed;top:20px;right:20px; background-color: white;"> <a href="https://daniel4-scratch.github.io/Forums/">Home</a></p>'
 
-function SearchBox(){
-  document.body.innerHTML += '<div class="box"> <center>Search</center> </div> <div class="box-content">Search is broken<br><input><button>Search</button></div>'
+function endBox(){
+   
+  setTimeout(function(){ document.body.innerHTML += '<br><div class="box"> <center>Footer </center> </div> <div class="box-content">Search is broken<br><input><button>Search</button></div>' }, 1000);
   // <div class="box"> <center>Forums</center> </div> <div class="box-content"> Pinned Forums: <a href="topics/Daniel4-Scratch-Info-Post">Info Post</a> </div>
 }
-SearchBox()
+()
