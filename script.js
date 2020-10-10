85c7ae1de3f65c6e569d1079fa51fcc82c469dbb
Daniel <mineminecraftpe687@gmail.com>
2020-10-10 17:16:24 +1100

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ function endBox(){
   setTimeout(function(){ document.body.innerHTML += '<br><div class="box"> <center>Footer </center> </div> <div class="box-content">Search is broken<br><input><button>Search</button></div>' }, 1000);
   // <div class="box"> <center>Forums</center> </div> <div class="box-content"> Pinned Forums: <a href="topics/Daniel4-Scratch-Info-Post">Info Post</a> </div>
 }
-()
+endBox()
