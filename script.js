529434221a451883c1d8906c0b062641f26515f0
Daniel <mineminecraftpe687@gmail.com>
2020-10-10 16:31:41 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.body.innerHTML += '<p style="position:fixed;top:20px;right:20px; background-color: grey;"> <a href="https://daniel4-scratch.github.io/Forums/">Home</a></p>'
+document.body.innerHTML += '<p style="position:fixed;top:20px;right:20px; background-color: white;"> <a href="https://daniel4-scratch.github.io/Forums/">Home</a></p>'
