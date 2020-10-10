7c476053a6f13807054752123a8d335394716f75
Daniel <mineminecraftpe687@gmail.com>
2020-10-10 20:12:57 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.body.innerHTML += '<p style="position:fixed;top:20px;right:20px; background-color: white;"> <a href="https://daniel4-scratch.github.io/Forums/">Home</a></p>'
-
-
-
+function searchf(){ 
+   
+   var number=document.getElementById("db-forums").value;  
+   window.location.href='topics/'+number+''
+} 
 function endBox(){
    
   setTimeout(function(){ document.body.innerHTML += '<br><p style="margin-left:9em; margin-right:9em;"><img src="https://cdn.pixabay.com/photo/2012/04/24/23/17/rss-41072_1280.png" width="15"> Feed: '+ window.location.pathname +' <span style="float:right;">'+document.title+'</span></p><br><div class="box"> <center>Footer </center> </div> <div class="box-content">Search<br><input type="text" id="db-forums" placeholder="Enter Text"/> <input type="button" value="Search" onclick="searchf()"/><br>or<br>Browse for a post <a href="https://github.com/Daniel4-Scratch/Forums/tree/main/topics">here</a>.</div>' }, 1000);
