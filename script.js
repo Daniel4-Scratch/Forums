635819f86cb25cad5621f5a5b78dd902038a02c4
Daniel <mineminecraftpe687@gmail.com>
2020-10-10 17:59:40 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.body.innerHTML += '<p style="position:fixed;top:20px;right:20px; backgr
 
 function endBox(){
    
-  setTimeout(function(){ document.body.innerHTML += '<br><div class="box"> <center>Footer </center> </div> <div class="box-content">Search is broken<br><input><button>Search</button><br>or<br>Browse for a post <a href="https://github.com/Daniel4-Scratch/Forums/tree/main/topics">here</a>.</div>' }, 1000);
+  setTimeout(function(){ document.body.innerHTML += '<br><p>Feed:'+ window.location.pathname +'</p><br><div class="box"> <center>Footer </center> </div> <div class="box-content">Search is broken<br><input><button>Search</button><br>or<br>Browse for a post <a href="https://github.com/Daniel4-Scratch/Forums/tree/main/topics">here</a>.</div>' }, 1000);
   // <div class="box"> <center>Forums</center> </div> <div class="box-content"> Pinned Forums: <a href="topics/Daniel4-Scratch-Info-Post">Info Post</a> </div>
 }
 endBox()
